1b8f9b55ce6b43a0dc9f61f9f71051c049f3104b
nick fallon <nick.fallon@centralfax.co.uk>
2023-08-08 01:24:53 +0100

    generator creates openapi json file

--- app/generator.js
+++ app/generator.js
@@ -7,19 +7,33 @@ module.exports = {
 
     build: async (req, res) => {
 
+        //create stub openAPI object
+
+        let openapi_object = module.exports.create_stub_openapi_object();
+
         //get public tables
 
         let tables = await module.exports.get_public_tables();
 
+        //sort table names
+
+        let table_names = [];
+        for (var table of tables) {
+            table_names.push(table.tablename);
+        }
+        table_names.sort();
+
         //create entities
 
-        for (var table of tables) {
-
-            await module.exports.create_entity(table);
-
+        for (table of table_names) {
+            await module.exports.create_entity(table, openapi_object);
         }
 
-        //create openAPI json file
+        //write the openapi json file
+
+        let api_json_path = path.join(__dirname, '../');
+        let api_json_file = `${api_json_path}/openapi.3.0.0.json`;
+        fs.writeFileSync(api_json_file, JSON.stringify(openapi_object));
 
         res.json({ msg: 'build complete' });
 
@@ -37,13 +51,21 @@ module.exports = {
 
     },
 
-    create_entity: async (table) => {
+    create_entity: async (table_name, openapi_object) => {
 
         // get the entity name and a wrapped (double-quoted) version if the table 
         // name is reserved
 
-        let table_name = table.tablename;
         let wrapped_table_name = module.exports.wrap_reserved_table_names(table_name);
+
+        //add the tablename as a tag to the openapi_object
+
+        openapi_object.tags.push(
+            {
+                description: table_name,
+                name: table_name
+            }
+        );
 
         // get foreign keys for entity
 
@@ -70,7 +92,11 @@ module.exports = {
 
         scripts.push(module_start);
 
-        scripts.push(module.exports.create_script_get_by_id(wrapped_table_name));
+        let { description, api_method_path, rest_method, api_method, parameters, script } = module.exports.generate_script_get_by_id(table_name, wrapped_table_name);
+
+        module.exports.attach_path_to_openapi_object(openapi_object, table_name, description, api_method_path, rest_method, api_method, parameters, script);
+
+        scripts.push(script);
 
         scripts.push(module_end);
 
@@ -78,6 +104,145 @@ module.exports = {
 
         let api_entity_codefile_path = `${api_entity_path}/index.js`;
         fs.writeFileSync(api_entity_codefile_path, scripts.join(''));
+
+    },
+
+    attach_path_to_openapi_object: (openapi_object, table_name, description, api_method_path, rest_method, api_method, parameters, script) => {
+
+        openapi_object.paths[api_method_path] = {
+        }
+
+        openapi_object.paths[api_method_path][rest_method] = {
+            parameters: parameters,
+            description: description,
+            operationId: `${table_name}/${api_method}`,
+            summary: description,
+            responses: {
+                200: {
+                    description: "successful operation"
+                },
+                400: {
+                    description: "Bad Request"
+                }
+            },
+            security: [
+                {
+                    bearerAuth: []
+                }
+            ],
+        }
+
+
+    },
+
+    generate_script_get_by_id: (table_name, wrapped_table_name) => {
+
+
+        let description = `Get ${table_name} by id`;
+        let api_method_path = `/${table_name}/{id}`;
+        let rest_method = 'get';
+        let parameters = [
+            {
+                "description": `${table_name} id`,
+                "in": "path",
+                "name": "id",
+                "required": true,
+                "schema": {
+                    "type": "integer"
+                }
+            }
+        ];
+
+        let api_method = `get_by_id`;
+
+        let script = `
+            ${api_method}: async (req, res) => {
+
+                try {
+                    let id = parseInt(req.params.id);
+                    let result = await module.exports.get_by_id_p(id);
+                    res.json(result);
+                }
+                catch (e){
+                    res.json(e);
+                }
+
+            },
+
+            ${api_method}_p: (id) => {
+
+                let sql = \`
+                    select * from ${wrapped_table_name}
+                    where id = $1;
+                \`;
+
+                let parameters = [id];
+                return db.query_promise(sql, parameters);
+
+            }
+        `;
+
+        return { description, api_method_path, rest_method, api_method, parameters, script }
+
+    },
+
+    create_stub_openapi_object: () => {
+
+        let stub_openapi_object = {
+            openapi: "3.0.0",
+            info: {
+                description: "OpenAPI 3.0 description",
+                title: "OpenAPI 3.0 title",
+                version: "1.0.0"
+            },
+            paths: {
+                "/": {
+                    get: {
+                        responses: {
+                            200: {
+                                "description": "successful operation"
+                            },
+                            400: {
+                                "description": "Bad Request"
+                            }
+                        },
+                        security: [
+                            {
+                                "bearerAuth": []
+                            }
+                        ],
+                        tags: [
+                            "Test"
+                        ],
+                        description: "Ping test, returns 200 OK",
+                        operationId: "health/ping",
+                        summary: "Return a HTTP 200 OK to check the application is available."
+                    }
+                }
+            },
+            tags: [
+                {
+                    description: "Test",
+                    name: "Test"
+                }
+            ],
+            servers: [
+                {
+                    url: "/api/v1"
+                }
+            ],
+            components: {
+                securitySchemes: {
+                    bearerAuth: {
+                        type: "http",
+                        scheme: "bearer"
+                    }
+                },
+                schemas: {}
+            }
+        }
+
+        return stub_openapi_object;
 
     },
 
@@ -90,7 +255,6 @@ module.exports = {
         return table_name;
 
     },
-
 
     get_fks_for_table: (table_name) => {
 
@@ -115,42 +279,6 @@ module.exports = {
         let parameters = [table_name];
         return db.query_promise(sql, parameters);
 
-    },
-
-    create_script_get_by_id: (table_name) => {
-
-        let script = `
-            get_by_id: async (req, res) => {
-
-                try {
-                    let id = parseInt(req.params.id);
-                    let result = await module.exports.get_by_id_p(id);
-                    res.json(result);
-                }
-                catch (e){
-                    res.json(e);
-                }
-
-            },
-
-            get_by_id_p: (id) => {
-
-                let sql = \`
-                    select * from ${table_name}
-                    where id = $1;
-                \`;
-
-                let parameters = [id];
-                return db.query_promise(sql, parameters);
-
-            }
-        `;
-
-        return script;
-
     }
-
-
-
 
 }
