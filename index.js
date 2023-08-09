274098e7af40a1d11627b3eca182d2f768f25699
nick fallon <nick.fallon@centralfax.co.uk>
2023-08-09 01:45:22 +0100

    comments

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
 
 require('dotenv').config();
 
-const port = process.env.PORT || "9000";
+// express
 
-//express
 const express = require('express');
 const compression = require('compression');
 const bodyParser = require("body-parser");
@@ -16,9 +15,14 @@ app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // start server
+
 let server;
+const port = process.env.PORT || "9000";
+
 if (process.env.localhost == 'true') {
-    //localhost SSL server
+
+    // use localhost SSL server
+
     const https = require("https");
     const fs = require("fs");
     const key = fs.readFileSync("certs/localhost-key.pem", "utf-8");
@@ -27,24 +31,32 @@ if (process.env.localhost == 'true') {
     console.log(`express running on: https://localhost:${port}`);
 }
 else {
-    //prod
+
+    // live (https)
+
     server = app.listen(port, () => { });
 }
 
 
-//api code generator
+// api code generator. use this to generate the api code 
+// and openapi specification on the first run.
+
 let generator = require('./app/generator');
 app.get('/api/generator/build', generator.build);
 
-//try to consume the openapi specification, if it exists
+// try to consume the openapi specification, 
+// if it exists (on second run)
+
 try {
 
-    //openapi
+    // get openapi json doc
+
     const openapi_json_path = "./openapi.3.0.0.json"
     const openAPIDef = require(openapi_json_path);
     const apiPath = openAPIDef.servers[0].url;
 
-    //openapi validation
+    // enforce openapi validation
+
     const OpenApiValidator = require('express-openapi-validator');
     app.use(
         OpenApiValidator.middleware({
@@ -54,6 +66,9 @@ try {
             ignorePaths: (path) => path.includes('/api-docs/')
         }),
     );
+
+    // bounce openapi validation errors
+
     app.use((err, req, res, next) => {
         res.status(err.status || 500).json({
             message: `openapi-validator: ${err.message}`,
@@ -61,7 +76,8 @@ try {
         });
     });
 
-    //openapi interactive swagger docs
+    // serve openapi interactive swagger docs at /api/v1/api-docs
+
     const swaggerUi = require("swagger-ui-express");
     const swaggerConfig = require("./routes/swaggerUI/config");
     app.use(
@@ -70,7 +86,8 @@ try {
         swaggerUi.setup(openAPIDef, swaggerConfig)
     );
 
-    //openapi routes
+    // create express routes for api based on the openapi doc
+
     let routes = require("./routes");
     routes.create_api_routes(app, openAPIDef, apiPath);
 
